d2b977b78ee163bdf2066be6a354683ddabaa7cf
Ela <elayudhanira@gmail.com>
2020-11-21 14:14:20 +0100

    Update Ibadah Natal 2020

--- page-templates/ibadah-natal.php
+++ page-templates/ibadah-natal.php
@@ -18,14 +18,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="full-width-page-wrapper">
 
 	<div class="container-fluid" id="content">
-	  <div class="col-md-12">
-		  <img src="http://grii-stockholm.org/wp-content/uploads/2019/11/natal2019.png" class="img-fluid" alt="Responsive image">
-	  </div>
+		<div class="row" style="background-color:#e4e4c1;">
+			<div class="col-md-12">
+			  <img src="http://grii-stockholm.org/wp-content/uploads/2020/11/Web_Header_e4e4c1.png" class="img-fluid" alt="Responsive image">
+			</div>
+		</div>		
+
 	  <div class="row">
       <div class="col-md-12 cover-2">
         <div class="container">
           <h1>IBADAH</h1>
-          <h1>NATAL 2019</h1>
+          <h1>NATAL 2020</h1>
         </div>
       </div>
 
@@ -34,17 +37,18 @@ $container = get_theme_mod( 'understrap_container_type' );
           <div class="row">
             <div class="col-md-6">
               <p class="lead">
-                Tema Natal di PRII/MRII di seluruh negara-negara Eropa adalah "The Condescension of God", membahas bagaimana Allah merendahkan diri menjadi manusia.
-              </p>
-              <p class="lead">
-                Rangkaian acara Natal akan dimulai dengan makan siang dan dilanjutkan dengan ibadah Natal. Saudara/saudari diundang hadir dan mengajak teman dan kerabatnya. Bagi saudara/saudari yang ingin berbagian, dapat mengisi form ini.
+                Tema Natal di PRII/MRII di seluruh negara-negara Eropa adalah "Roh TUHAN ada pada-Nya".
+		<br/>
+		 <em>Yesaya 11:2</em>
+		 <br/>
+		<em>"Roh TUHAN akan ada padanya, roh hikmat dan pengertian, roh nasihat dan keperkasaan, roh pengenalan dan takut akan TUHAN;"</em>
               </p>
             </div>
             <div class="w-100"></div>
 
             <div class="col-md-4">
               <h6 class="paragraph-display">Pembicara</h6>
-              <p class="paragraph-title">Vic. Jack Kawira</p>
+              <p class="paragraph-title">Pdt. Billy Kristanto</p>
 
               <h6 class="paragraph-display">Info</h6>
               <p class="paragraph-title">Christina Lia - 073 675 1107.</p>
@@ -52,10 +56,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
             <div class="col-md-4">
               <h6 class="paragraph-display">Tempat</h6>
-              <p class="paragraph-title">Birkagården, Karlbergsvägen 86B</p>
+              <p class="paragraph-title">Birkagården, Karlbergsvägen 86B <br/> <a href="https://goo.gl/maps/9d2B9WvFTJdVeTkF8" target="_blank">Peta Lokasi</a></p>
 
               <h6 class="paragraph-display">Waktu</h6>
-              <p class="paragraph-title">Minggu, 1 Desember 2019, 13:30</p>
+              <p class="paragraph-title">Minggu, 20 Desember 2020, 10:00</p>
             </div>
 
           </div>
